b8f4e060d4f18614a72e0450deee4879908596e1
Jackson Asprilla <con_jasprilla@credicorpbank.com>
2022-12-21 11:51:37 -0500

    Add methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        int[] c = {1,1,1,0, 1, 1, 0,0, 0, 0 };
+        //int[] c = {1,1,1,0, 1, 1, 0,0, 0, 0 };
         //int[] c = {0,0,1,0};
         //int[] c = {0,0,1,0,0,1,1,0};
-        int k =3;
-        System.out.println(jumpingOnClouds(c, k));
+        //int k =3;
+        //System.out.println(jumpingOnClouds(c, k));
+        //System.out.println(findDigits(110110015));
+        extraLongFactorials(25);
     }
 
     static int jumpingOnClouds(int[] c, int k) {
@@ -17,5 +21,35 @@ public class Main {
             i = i + k;
         }
         return e;
+    }
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        char[] numbers = String.valueOf(n).toCharArray();
+        int count = 0;
+        int divisor = 0;
+        for(char i : numbers){
+            if(i != '0'){
+                divisor = n % Character.getNumericValue(i);
+                if(divisor == 0){
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void extraLongFactorials(int n) {
+        BigInteger factorial = BigInteger.ONE;
+        for( int i = n; i >= 1; i --){
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.print(factorial);
     }
 }
