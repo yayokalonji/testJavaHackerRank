f897b96b26e5a3ed001bc94b2ffa2bb3a3ecd4d7
Jackson Asprilla <yayokalonji@icloud.com>
2023-12-01 15:43:22 -0500

    Add more challenges

--- src/Main.java
+++ src/Main.java
@@ -101,6 +101,10 @@ public class Main {
         out.println(Arrays.toString(countBy(3, 9)));
         out.println(Arrays.toString(digitize(123)));
         out.println(seriesSum(2));
+        out.println(isSquare(26));
+        out.println(findIt(new int[]{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}));
+        out.println(Arrays.toString(dirReduc(new String[]{"EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST"})));
+        out.println(Arrays.toString(twoSum(new int[]{1, 2, 3}, 4)));
     }
 
     static int jumpingOnClouds(int[] c, int k) {
@@ -1104,5 +1108,85 @@ public class Main {
             sum += 1.0 / (3 * i - 2);
         }
         return String.format("%.2f", sum);
+    }
+
+    /*
+    * A square of squares
+    * */
+    public static boolean isSquare(int n) {
+        return Math.sqrt(n) % 1 == 0;
+    }
+
+    public static int findIt(int[] a) {
+        int result = 0;
+        for (int j : a) {
+            result ^= j;
+        }
+        return result;
+    }
+
+    /*
+    * Once upon a time, on a way through the old wild mountainous west,…
+    *  a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+    * Going to one direction and coming back the opposite direction right away is a needless effort.
+    * Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy; otherwise you might die of thirst!
+    * How I crossed a mountainous desert the smart way.
+    * The directions given to the man are, for example, the following (depending on the language):
+    *
+    * ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+    * or
+    * { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+    * or
+    * [North, South, South, East, West, North, West]
+    *
+    * You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan.
+    * A better plan in this case is simple:
+    *
+    * ["WEST"]
+    * or
+    * { "WEST" }
+    * or
+    * [West]
+    *
+    * Other examples:
+    * In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+    * The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+    * In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+    * Task Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+    * The Haskell version takes a list of directions with data Direction = North | East | West | South.
+    * The Clojure version returns nil when the path is reduced to nothing.The Rust version takes a slice of enum Direction {North, East, West, South}.
+    * See more examples in "Sample Tests:"
+    * Notes
+    * Not all paths can be made simpler. The path ["NORTH", "WEST", "SOUTH", "EAST"] is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH",
+    * "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : ["NORTH", "WEST", "SOUTH", "EAST"].
+    * if you want to translate, please ask before translating.
+    * */
+    public static String[] dirReduc(String [] arr){
+        Map<String, String> opposites = new HashMap<>();
+        opposites.put("NORTH", "SOUTH");
+        opposites.put("SOUTH", "NORTH");
+        opposites.put("EAST", "WEST");
+        opposites.put("WEST", "EAST");
+        Stack<String> stack = new Stack<>();
+        for (String direction : arr) {
+            if (stack.isEmpty() || !stack.peek().equals(opposites.get(direction))) {
+                stack.push(direction);
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.toArray(String[]::new);
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int [] result = {};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] + numbers[i] == target) {
+                    result = new int[]{i, j};
+                }
+            }
+        }
+        return result;
     }
 }
